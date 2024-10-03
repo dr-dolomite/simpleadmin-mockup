edb165a82612afc221bc0f99f57211f521238090
Russel Yasol <russel.yasol@wvsu.edu.ph>
2024-10-03 19:55:50 +0800

    updated ttl script

--- js/advance/fetch-current.settings.js
+++ js/advance/fetch-current.settings.js
@@ -13,9 +13,43 @@ async function fetchCurrentSettings() {
     }
 }
 
-fetchCurrentSettings().then(data => {
-    if (data) {
-        // Update the UI with the current settings
-        console.log('Updating the UI with current settings:', data);
-    }
-});
+function init() {
+    fetchCurrentSettings().then(data => {
+        if (data) {
+            // Parse the data and update the UI
+
+            // Passthrough Mode
+            const mpdnRuleLine = data[0].response.split('\n')[1];
+            if (mpdnRuleLine) {
+                const mpdnRule = mpdnRuleLine.split(":")[1].trim();
+                switch (mpdnRule) {
+                    case '"MPDN_rule",0,0,0,0,0':
+                        document.getElementById('ip-passthrough-mode').value = 'Disabled';
+                        break;
+                    case '"MPDN_rule",0,1,0,1,1':
+                        document.getElementById('ip-passthrough-mode').value = 'ETH Only';
+                        break;
+                    case '"MPDN_rule",0,1,0,3,1':
+                        document.getElementById('ip-passthrough-mode').value = 'USB Only';
+                        break;
+                    default:
+                        document.getElementById('ip-passthrough-mode').value = 'Select IP Passthrough Mode';
+                        break;
+                }
+            }
+
+            // DNS Proxy
+            const dnsProxyLine = data[1].response.split('\n')[1].split(":")[1].split(",")[1].trim();
+            if (dnsProxyLine) {
+                dnsProxyLine === '"disable"' ? document.getElementById('dns-proxy-mode').value = 'Disabled' : document.getElementById('dns-proxy-mode').value = 'Enabled';
+            } else {
+                document.getElementById('dns-proxy-mode').value = 'Select Onboard DNS Proxy';
+            }
+        } else {
+            alert('Failed to fetch current settings. Please refresh the page.');
+        }
+    });
+}
+
+// run init when the DOM is ready and page is loaded
+document.addEventListener('DOMContentLoaded', init);
